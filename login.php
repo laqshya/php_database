9ea1a0aa757ef47c24fd6aaba33992a1917769d4
lasqshya <laqshya97@gmail.com>
2019-08-03 13:40:08 +0530

    intended

--- login.php
+++ login.php
@@ -5,42 +5,43 @@ $password = "";
 
 if(empty($_POST["user_name"]) or empty($_POST["password"]) ) {
 	echo 'one of the required field is empty';
-}	
+}
+	
 else {
 	$conn = new mysqli($servername, $username, $password);
-if ($conn->connect_error) {
-	die("connection failed:" .$conn->connect_error);
-}
+	
+	if ($conn->connect_error) {
+		die("connection failed:" .$conn->connect_error);
+	}
 
-else {
+    else {
 
-function test_input($data)
-{
+    function test_input($data) {
 	$data = trim($data);
 	$data = stripslashes($data);
 	$data = htmlspecialchars($data);
 	return $data;
-}
+	}
 
- mysqli_select_db($conn, 'register');
+	mysqli_select_db($conn, 'register');
 
- $Firstname = test_input($_POST["user_name"]);
- $Password = test_input($_POST["password"]);
+	$Firstname = test_input($_POST["user_name"]);
+	$Password = test_input($_POST["password"]);
  
  
  
-  $result = mysqli_query($conn, "SELECT * FROM accounts WHERE First_name = '$Firstname' AND Password = '$Password'") or die("Fail to query.".mysqli_error($conn));
+	$result = mysqli_query($conn, "SELECT * FROM accounts WHERE First_name = '$Firstname' AND Password = '$Password'") or die("Fail to query.".mysqli_error($conn));
   
-  $row = mysqli_fetch_array($result);
+	$row = mysqli_fetch_array($result);
   
-  if($row['First_name'] == $Firstname && $row['Password'] == $Password) {
-	  echo "LOGIN SUCCESSFULLY";  
-  }
-  else {
-	  echo "Fail to log in";
-  }
+	if($row['First_name'] == $Firstname && $row['Password'] == $Password) {
+		echo "LOGIN SUCCESSFULLY";  
+	}
+	else {
+		echo "Fail to log in";
+	}
  
-}
+	}
 }
  
 ?>
